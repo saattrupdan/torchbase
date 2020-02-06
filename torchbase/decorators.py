a6e1760a6d3af8471bcae8bec805d7152c1b188b
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-02-06 15:57:19 +0000

    feat: Add changename decorator

--- torchbase/decorators.py
+++ torchbase/decorators.py
@@ -1,7 +1,5 @@
 import torch
 from pathlib import Path
-from .modules import ModuleWrapper
-from .utils import Metric
 from .typing import *
 
 def parametrised(decorator: Decorator) -> Decorator:
@@ -13,20 +11,27 @@ def parametrised(decorator: Decorator) -> Decorator:
     return parametrised_decorator
 
 @parametrised
+def changename(f: Function, name: str) -> Function:
+    f.__name__ = name
+    return f
+
+@parametrised
 def magic(cls: Module,
     criterion: Metriclike = 'binary_cross_entropy',
     optimiser: Optimiserlike = 'adamw',
     scheduler: nSchedulerlike = 'reduce_on_plateau',
-    metrics: Metriclikes = [],
+    metrics: MetriclikesOrString = [],
     data_dir: Pathlike = '.data',
     verbose: int = 1) -> Wrapper:
     ''' Adds more functionality to a PyTorch Module. '''
+    from .modules import ModuleWrapper
 
     class MagicModule(ModuleWrapper):
         _model_class = cls
         _optimiser = optimiser
         _scheduler = scheduler
         _criterion = criterion
+        _metrics = metrics
         _data_dir = data_dir
         _verbose = verbose
 
