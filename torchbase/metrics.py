74f1f18a0e2b63fda8a1c4d7f9cc8fbd8e92a468
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-02-06 15:57:36 +0000

    feat: Add metric class

--- torchbase/metrics.py
+++ torchbase/metrics.py
@@ -1,14 +1,41 @@
 import torch
+from .decorators import changename
 from .typing import *
 
-def accuracy(pred: Tensor, true: Tensor) -> float:
+class Metric:
+    def __init__(self, metric: Metriclike, wrapper: Wrapper, val: bool):
+        from .utils import str2metric
+        self.metric = str2metric(metric, wrapper)
+
+        try:
+            self.name = self.metric.__name__.lower()
+        except AttributeError:
+            self.name = type(self.metric).__name__.lower()
+
+        if val: self.name = 'val_' + self.name
+
+    def __call__(self, pred: Tensor, true: Tensor) -> float:
+        return self.metric(pred, true)
+
+def accuracy(pred: FloatTensor, true: Tensor) -> float:
+    if pred.shape != true.shape:
+        true = torch.nn.functional.one_hot(true, num_classes = pred.shape[-1])
+    pred = torch.gt(pred, 0.5)
     correct = torch.eq(torch.sum(torch.eq(pred, ~true.bool()), dim = 1), 0)
     return torch.mean(correct.float()).item()
 
+@changename('accuracy')
+def accuracy_with_logits(pred: FloatTensor, true: Tensor) -> float:
+    return accuracy(torch.nn.functional.softmax(pred, dim = -1), true)
+
 def samples_f1(pred: Tensor, true: Tensor) -> float:
-    pred, true = pred.bool(), true.bool()
+    pred, true = torch.gt(pred, 0.5), torch.gt(true, 0.5)
     tp = torch.sum(pred & true, dim = 1).float()
     false_preds = torch.sum(torch.eq(pred, ~true), dim = 1).float()
     denom = 2 * tp + false_preds
     denom = torch.where(denom == 0, torch.ones(denom.size()), denom)
     return torch.mean(2 * tp / denom).item()
+
+@changename('samples_f1')
+def samples_f1_with_logits(pred: Tensor, true: Tensor) -> float:
+    return samples_f1(torch.sigmoid(pred), true)
