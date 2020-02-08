bdf6f956b8dc11b71c96c395a4dd1d348c2e1984
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-02-08 13:07:28 +0000

    feat: Add tensorboard

--- torchbase/training.py
+++ torchbase/training.py
@@ -5,27 +5,28 @@ from functools import cmp_to_key
 from .utils import str2sched
 from .typing import *
 
-def train_model(wrapper: Wrapper,
-    train_loader: DataLoader, 
-    val_loader: nDataLoader = None,
-    epochs: nNumeric = None,
-    monitor: nStr = None,
-    minimise_monitor: nBool = None,
-    target_value: nFloat = None,
-    patience: nNumeric = 10,
-    smoothing: float = 0.99, 
-    pred_threshold: float = 0.5, 
-    save_model: bool = True,
-    overwrite: bool = True) -> Wrapper:
+def train_model(wrapper: Wrapper, train_loader: DataLoader, 
+    val_loader: nDataLoader = None, epochs: nNumeric = None) -> Wrapper:
+
+    # Initialise TensorBoard and add model graph
+    if wrapper.tensorboard:
+        from torch.utils.tensorboard import SummaryWriter
+        writer = SummaryWriter()
+        writer.add_graph(wrapper.model, next(iter(train_loader))[0])
+        wrapper.logger.info('Tensorboard active! Run "tensorboard '\
+            '--logdir=runs" in a new terminal window to open it.')
 
     # Alias the loss function
     crit = wrapper.criterion
 
     # Set epochs and patience to infinity if they are None
-    if epochs is None: epochs = float('inf')
-    if patience is None: patience = float('inf')
-    if monitor is None:
+    epochs = float('inf') if epochs is None else epochs
+    patience = float('inf') if wrapper.patience is None else wrapper.patience
+    if wrapper.monitor is None:
         monitor = 'loss' if val_loader is None else 'val_loss'
+    else:
+        monitor = wrapper.monitor
+    wrapper.logger.debug(f'Monitoring {monitor}')
 
     # Initialise scheduler
     if isinstance(wrapper.scheduler, str):
@@ -35,6 +36,8 @@ def train_model(wrapper: Wrapper,
             epochs = epochs,
             patience = patience
         )
+        wrapper.logger.debug('Initialised '\
+            f'{type(wrapper.scheduler).__name__} scheduler')
 
     # Bundle together the metrics and their names and values
     metric_data = {
@@ -45,21 +48,27 @@ def train_model(wrapper: Wrapper,
         }
         for metric in wrapper.metrics
     }
+    wrapper.logger.debug('Initialised metrics '\
+        f'{[m.name for m in wrapper.metrics]}')
 
     # Initialise minimise_monitor
-    if minimise_monitor is None:
+    if wrapper.minimise_monitor is None:
         if monitor == 'loss' or monitor == 'val_loss':
             minimise_monitor = True
         else:
             minimise_monitor = False
+    else:
+        minimise_monitor = wrapper.minimise_monitor
 
     # Initialise score_cmp and best_score
     if minimise_monitor:
         score_cmp = lambda x, y: x < y
         best_score = float('inf')
+        wrapper.logger.debug(f'Minimising {monitor}')
     else:
         score_cmp = lambda x, y: x > y
         best_score = 0.
+        wrapper.logger.debug(f'Maximising {monitor}')
 
     # Initialise history
     for name in metric_data.keys():
@@ -67,6 +76,7 @@ def train_model(wrapper: Wrapper,
             wrapper.history[name] = []
         elif wrapper.history[name]:
             metric_data[name]['value'] = wrapper.history[name][-1]
+    wrapper.logger.debug('History initialised')
 
     # Initialise scores.
     # We're working with all scores rather than the current score since
@@ -74,9 +84,11 @@ def train_model(wrapper: Wrapper,
     # the score on the fly
     scores = wrapper.history[monitor]
     best_score = max([best_score] + scores, key = cmp_to_key(score_cmp))
+    wrapper.logger.debug(f'Best score initialised to {best_score}')
 
     # Initialise starting epoch
     start_epoch = len(wrapper.history['loss'])
+    wrapper.logger.debug(f'Starting epoch initialised to {start_epoch}')
 
     # Initialise the number of bad epochs; i.e. epochs with no improvement
     bad_epochs = 0
@@ -94,10 +106,8 @@ def train_model(wrapper: Wrapper,
                      f'{wrapper.trainable_params():,d}')
 
     # Training loop
+    niter: int = 0
     for epoch in count(start = start_epoch):
-
-        # Epoch announcement
-        wrapper.logger.debug(f'Commencing epoch {epoch}')
 
         # Enable training mode
         wrapper.train()
@@ -112,6 +122,9 @@ def train_model(wrapper: Wrapper,
         with tqdm(total = nsamples) as epoch_pbar:
             epoch_pbar.set_description(f'Epoch {epoch:2d}')
             for idx, (xtrain, ytrain) in enumerate(train_loader):
+
+                # Update the number of iterations
+                niter += train_loader.batch_size
 
                 # Reset the gradients
                 wrapper.optimiser.zero_grad()
@@ -144,17 +157,21 @@ def train_model(wrapper: Wrapper,
                             value = data['metric'](yhat.detach(), ytrain)
 
                         # Moving average
-                        value = smoothing * data['value'] + \
-                                (1 - smoothing) * value
+                        value = wrapper.smoothing * data['value'] + \
+                                (1 - wrapper.smoothing) * value
 
                         # Bias correction
-                        exponent: float = epoch * nsamples 
-                        exponent += (idx + 1) * train_loader.batch_size
-                        exponent /= 1 - smoothing
-                        value /= 1 - smoothing ** exponent
+                        value /= 1 - wrapper.smoothing ** \
+                                 (niter / (1 - wrapper.smoothing))
 
                         # Update the metric value
                         metric_data[name]['value'] = value
+
+                        # Update Tensorboard
+                        if wrapper.tensorboard:
+                            wrapper.logger.debug(f'Adding {name} to '\
+                                                 f'Tensorboard')
+                            writer.add_scalar(name + '/train', value, niter)
 
                 # Update progress bar description
                 desc = f'Epoch {epoch:2d}'
@@ -167,10 +184,12 @@ def train_model(wrapper: Wrapper,
             # Add training scores to history
             for name, data in metric_data.items():
                 if data['train']:
+                    wrapper.logger.debug(f'Adding {name} to history')
                     wrapper.history[name].append(data['value'])
 
             # Compute validation metrics
             if val_loader is not None:
+                wrapper.logger.debug('Computing validation metrics')
                 with torch.no_grad():
 
                     # Enable validation mode
@@ -194,7 +213,15 @@ def train_model(wrapper: Wrapper,
                     # Add training scores to history
                     for name, data in metric_data.items():
                         if not data['train']:
+                            wrapper.logger.debug(f'Adding {name} to history')
                             wrapper.history[name].append(data['value'])
+
+                            # Update Tensorboard
+                            if wrapper.tensorboard:
+                                wrapper.logger.debug(f'Adding {name} to '\
+                                                     f'Tensorboard')
+                                name = name[4:] + '/val'
+                                writer.add_scalar(name, data['value'], niter)
 
                     # Update progress bar description
                     desc = f'Epoch {epoch:2d}'
@@ -205,41 +232,56 @@ def train_model(wrapper: Wrapper,
         # Add score to learning scheduler
         if wrapper.scheduler is not None: wrapper.scheduler.step(scores[-1])
 
-        # Save model if score is best so far
-        if score_cmp(scores[-1], best_score):  
-            best_score = scores[-1]
+        # Update Tensorboard
+        if wrapper.tensorboard:
+            wrapper.logger.debug('Adding learning rate to Tensorboard')
+            lr: float = wrapper.optimiser.param_groups[0]['lr']
+            writer.add_scalar('learning_rate', lr, niter)
 
-        # Delete older models and save the current one
-        if save_model:
-            if overwrite:
-                for f in wrapper.data_dir.glob(f'{wrapper.model_name}*.pt'): 
-                    f.unlink()
-            wrapper.save(f'{scores[-1]:.4f}_{monitor}')
+        # Save model if score is best so far
+        if score_cmp(round(scores[-1], 4), round(best_score, 4)):
+            best_score = scores[-1]
+            wrapper.logger.debug(f'Best score is now {best_score:.4f}')
+
+            # Delete older models and save the current one
+            if wrapper.save_model:
+                wrapper.logger.debug('Saving model')
+                if wrapper.overwrite:
+                    files = wrapper.data_dir.glob(f'{wrapper.model_name}*.pt')
+                    for f in files: f.unlink()
+                wrapper.save(f'{scores[-1]:.4f}_{monitor}')
+
+            # Reset number of bad epochs
+            if bad_epochs:
+                wrapper.logger.debug('Bad epochs reset')
+            bad_epochs = 0
 
         # Stop if score has not improved for <patience> many epochs
-        if score_cmp(best_score, scores[-1]):
+        else:
             bad_epochs += 1
+            if bad_epochs == 1:
+                wrapper.logger.debug(f'There is now {bad_epochs} bad epoch')
+            else:
+                wrapper.logger.debug(f'There are now {bad_epochs} bad epochs')
+
             if bad_epochs > patience:
                 wrapper.logger.info('Model is not improving, stopping '\
-                                 'training.')
+                                    'training.')
 
                 # Load the model with the best score
                 glob = list(wrapper.data_dir.glob(f'{wrapper.model_name}*.pt'))
-                if save_model and glob != []:
+                if wrapper.save_model and glob != []:
                     checkpoint = torch.load(glob[0])
                     wrapper.history = checkpoint['history']
                     wrapper.load_state_dict(checkpoint['model_state_dict'])
 
                 break
 
-        # If score *has* improved then reset <bad_epochs>
-        else: bad_epochs = 0
-
-        # Stop when we perfom better than <target_value>
-        if target_value is not None:
-            if score_cmp(scores[-1], target_value):
+        # Stop when we perfom better than target_value
+        if wrapper.target_value is not None:
+            if score_cmp(scores[-1], wrapper.target_value):
                 wrapper.logger.info('Reached target performance, stopping '\
-                                 'training.')
+                                    'training.')
                 break
 
     return wrapper
